4a32eb5e8a3fbf0285402b2e6e4d8168c8c84ff8
arslan-aziz <arslan.aziz@outlook.com>
2020-04-11 12:35:34 -0400

    cmd argument to specify server ip and port

--- node/main.py
+++ node/main.py
@@ -8,6 +8,7 @@ import picamera
 import time
 import io
 import base64
+import argparse
 
 def capture_img():
 	my_stream = io.BytesIO()
@@ -25,11 +26,14 @@ def post_image(URL):
 	response = requests.post(URL, data=img_encoded) #post string data
 	return response
 
+my_parser = argparse.ArgumentParser(description="Capture and Post images")
+my_parser.add_argument("ServerIP",type=str,help="ip addr of web server")
+my_parser.add_argument("Port",type=str,help="port on web server")
+args = my_parser.parse_args()
 
-if __name__ == "__main__":
-	URL ="http://192.168.1.12:80/upload_image" 
-	while(True):
-		response = post_image(URL)
-		print(str(response.status_code)+" "+response.reason)
-		time.sleep(2)
+URL ="http://{addr}:{p}/upload_image".format(addr=args.ServerIP,p=args.Port)
+while(True):
+	response = post_image(URL)
+	print(str(response.status_code)+" "+response.reason)
+	time.sleep(2)
 
