0bbaeab8e4c610ff6e794eab144bc9b397986283
Arslan Aziz <arslan.aziz@outlook.com>
2020-04-11 19:50:55 -0400

    replace uWSGI w/ gunicorn WSGI server which supports SSE

--- server/flask/app/__init__.py
+++ server/flask/app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from redis import StrictRedis
+
+#redis_store = FlaskRedis.from_custom_provider(StrictRedis)
+redis_store = StrictRedis.from_url("redis://redis:6379/0",decode_responses=True)
 
 app = Flask(__name__)
+#app.config['REDIS_URL'] = "redis://redis:6379/0"
 data = ""
+
+#redis_store.init_app(app)
+
 
 from app import views
--- server/flask/app/views.py
+++ server/flask/app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, request
+from app import redis_store
+from flask import render_template, request, Response
 import io
 import os
 import base64
@@ -7,30 +8,39 @@ import base64
 @app.route("/")
 def index():
     return render_template('main.html')
-    #return "Hello Flask!"
-
-# @app.route("/pitest",methods=['GET','POST'])
-# def test_pi():
-#     if(request.method == 'GET'):
-#         return data
-#     else:
-#         data = request.data.decode('utf-8')
-#     print(data)
-    
-@app.route("/show_image",methods=['GET'])
-def show_image():
-    return render_template('main.html')
 
 @app.route("/upload_image",methods=['POST'])
 def upload_image():
     print(type(request.data))
     img_bytes = base64.b64decode(request.data)
-    #buffer = io.BytesIO(request.data)
+    #TODO: this label will be assigned in the classifier service
     path = os.path.join('app','static','img','test.jpg')
     with open(path,'wb+') as f:
-        #f.write(buffer.getvalue())
         f.write(img_bytes)
+    redis_store.publish('label',str(redis_store.get('image_counter')))
+    redis_store.incr('image_counter')
     return "done"
-    #return render_template('main.html')
-    #img = Image.open(io.BytesIO(request.data))
+
+# @app.route("/image_count",methods=['GET'])
+# def get_count():
+#     return """
+#     <!doctype html>
+#     <TITLE> {c} </TITLE>
+#     <BODY> {c} </BODY>
+#     """.format(c=redis_store.get('image_counter'))
+
+
+@app.route("/stream")
+def stream():
+    def event_stream():
+        pubsub = redis_store.pubsub()
+        pubsub.subscribe('label')
+        for message in pubsub.listen():
+            print('message received')
+            yield 'data: {c}\n\n'.format(c=message['data'])
+    return Response(event_stream(),mimetype="text/event-stream")
+
+
+
+
 
